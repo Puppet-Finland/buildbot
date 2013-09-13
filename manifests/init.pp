51468aa7c613a9f93ac9b93e82d64653a06c7625
Samuli Seppänen <samuli@openvpn.net>
2013-09-13 19:52:34 +0000

    Added default values to Hiera checks
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -23,7 +23,7 @@
 class buildbot {
 
 # Rationale for this is explained in init.pp of the sshd module
-if hiera('manage_buildbot') != 'false' {
+if hiera('manage_buildbot', 'true') != 'false' {
 
     include buildbot::install
 }
